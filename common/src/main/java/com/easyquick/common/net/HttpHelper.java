b4032ad34981a8ce514a897c8d0108b5cea79ee0
wanui <wanui2003@163.com>
2014-06-26 17:31:43 +0800

    dd Signed-off-by: wanui <wanui2003@163.com>

--- common/src/main/java/com/easyquick/common/net/HttpHelper.java
+++ common/src/main/java/com/easyquick/common/net/HttpHelper.java
@@ -12,8 +12,10 @@ import javax.xml.crypto.Data;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -39,15 +41,43 @@ public class HttpHelper {
 	private final int HTTP_STATUS_SUCCESS = 200;
 	
 	private String urlPrefix;
+	private CloseableHttpClient httpClient;
+	private HttpClientBuilder httpClientBuilder;
+	private HttpClientContext httpContext;
+	private HttpGet httpGet;
+	private HttpPost httpPost;
 	
-	private CloseableHttpClient httpClient = HttpClientBuilder
-			.create()
-			.setUserAgent(USER_AGENT)
-			.build();
-	private HttpClientContext httpContext = HttpClientContext.create();  
-	private HttpGet httpGet = new HttpGet();
-	private HttpPost httpPost = new HttpPost();
 	
+	public HttpHelper() {
+		init();
+		httpClient =httpClientBuilder
+				.setUserAgent(USER_AGENT)
+				.build();
+	}
+	
+	public HttpHelper(String urlPrefix) {
+		init();
+		httpClient =httpClientBuilder
+				.setUserAgent(USER_AGENT)
+				.build();
+		this.urlPrefix = urlPrefix;
+	}
+	
+	public HttpHelper(String urlPrefix, RequestConfig requestConfig) {
+		init();
+		httpClient =httpClientBuilder
+				.setUserAgent(USER_AGENT)
+				.setDefaultRequestConfig(requestConfig)
+				.build();
+		this.urlPrefix = urlPrefix;
+	}
+	
+	private void init(){
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+		HttpClientContext httpContext = HttpClientContext.create();  
+		HttpGet httpGet = new HttpGet();
+		HttpPost httpPost = new HttpPost();
+	}
 	
 	/**
 	 * 用post方式发送请求,返回字符串
@@ -57,10 +87,18 @@ public class HttpHelper {
 	 * @throws Exception
 	 */
 	public String requestStringByPost(String url, List<? extends NameValuePair> data) throws Exception{
+		String result;
+		CloseableHttpResponse response = request(url, HttpMethod.POST, data);
 		
-		CloseableHttpResponse response = request(url, HttpMethod.POST, data, null);
-		
-		return EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+		try {
+			result = EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+			return result;
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			response.close();
+		}
 		
 	}
 	
@@ -72,12 +110,20 @@ public class HttpHelper {
 	 * @throws Exception
 	 */
 	public String requestXmlStringByPost(String url, List<? extends NameValuePair> data) throws Exception{
-		HashMap<String, String> header = new HashMap<>();
-		header.put("Content-Type", CONTENT_TYPE_XML);
-		CloseableHttpResponse response = request(url, HttpMethod.POST, data, header);
+		String result;
+		this.addHeader("Content-Type", CONTENT_TYPE_XML);
+		CloseableHttpResponse response = request(url, HttpMethod.POST, data);
 		
-		return EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
-		
+		try {
+			result = EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+			return result;
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			this.removeHeader("Content-Type", CONTENT_TYPE_XML);
+			response.close();
+		}
 	}
 	
 	/**
@@ -87,10 +133,18 @@ public class HttpHelper {
 	 * @throws Exception
 	 */
 	public String requestStringByGet(String url) throws Exception{
+		String result;
+		CloseableHttpResponse response = request(url, HttpMethod.GET, null);
 		
-		CloseableHttpResponse response = request(url, HttpMethod.GET, null, null);
-		
-		return EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+		try {
+			result = EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+			return result;
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			response.close();
+		}
 		
 	}
 	
@@ -100,13 +154,61 @@ public class HttpHelper {
 	 * @return String 请求返回字符串
 	 * @throws Exception
 	 */
-	public String requestXmlStringByGet(String url) throws Exception{
-		HashMap<String, String> header = new HashMap<>();
-		header.put("Content-Type", CONTENT_TYPE_XML);
-		CloseableHttpResponse response = request(url, HttpMethod.POST, null, header);
+	public String requestXmlStringByGet(String url) throws Exception{		
+		String result;
+		this.addHeader("Content-Type", CONTENT_TYPE_XML);
+		CloseableHttpResponse response = request(url, HttpMethod.GET, null);
 		
-		return EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+		try {
+			result = EntityUtils.toString(response.getEntity(), DEFAUT_CHARSET);
+			return result;
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			this.removeHeader("Content-Type", CONTENT_TYPE_XML);
+			response.close();
+		}
 		
+	}
+	
+	
+	/**
+	 * 用get方式下载文件
+	 * @param url 请求的url
+	 * @param localFileName 保存的文件名
+	 * @throws Exception
+	 */
+	public void downloadFileByGet(String url, String localFileName) throws Exception {
+		CloseableHttpResponse response = this.requestByGet(url);
+		try {
+			this.saveFile(response, localFileName);
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			response.close();
+		}
+		
+	}
+	
+	/**
+	 * 用post方式下载文件
+	 * @param url 请求的url
+	 * @param localFileName 保存的文件名
+	 * @param data 提交的数据
+	 * @throws Exception
+	 */
+	public void downloadFileByPost(String url, String localFileName, List<? extends NameValuePair> data) throws Exception {
+		CloseableHttpResponse response = this.requestByPost(url, data);
+		try {
+			this.saveFile(response, localFileName);
+		} catch (Exception e) {
+			throw e;
+		}
+		finally{
+			response.close();
+		}
 	}
 	
 	/**
@@ -116,10 +218,8 @@ public class HttpHelper {
 	 * @return CloseableHttpResponse 请求回应
 	 * @throws Exception
 	 */
-	public CloseableHttpResponse requestByPost(String url, List<? extends NameValuePair> data) throws Exception{
-		
-		return request(url, HttpMethod.POST, data, null);
-		
+	public CloseableHttpResponse requestByPost(String url, List<? extends NameValuePair> data) throws Exception{	
+		return request(url, HttpMethod.POST, data);
 	}
 	
 	/**
@@ -129,32 +229,27 @@ public class HttpHelper {
 	 * @throws Exception
 	 */
 	public CloseableHttpResponse requestByGet(String url) throws Exception{
-		
-		return request(url, HttpMethod.GET, null, null);
-		
+		return request(url, HttpMethod.GET, null);
 	}
-	
 	
 	/**
 	 * 发送请求
 	 * @param url 请求的url
-	 * @param data 提交的数据
 	 * @param httpMethod 请求的方法
+	 * @param data 提交的数据
 	 * @return CloseableHttpResponse 请求回应
 	 * @throws Exception
 	 */
-	public CloseableHttpResponse request(String url, HttpMethod httpMethod, List<? extends NameValuePair> data, HashMap<String, String> headers) throws Exception{
+	public CloseableHttpResponse request(String url, HttpMethod httpMethod, List<? extends NameValuePair> data) throws Exception{
 		URI uri = this.buildURI(url);
 		CloseableHttpResponse response = null;
 		
 		switch(httpMethod){
 			case GET:
-				this.addHeaders(httpGet, headers);
 				httpGet.setURI(uri);
 				response = httpClient.execute(httpGet, httpContext);
 				break;
 			case POST:
-				this.addHeaders(httpPost, headers);
 				httpPost.setURI(uri);
 				httpPost.setEntity(new UrlEncodedFormEntity(data, DEFAUT_CHARSET));
 				response = httpClient.execute(httpPost, httpContext);
@@ -172,22 +267,114 @@ public class HttpHelper {
 	
 	/**
 	 * 关闭HttpHelper释放资源
-	 * @throws Exception
+	 * @throws Exceptionddddd
 	 */
 	public void close() throws Exception{
 		httpClient.close();
 	}
 	
+	/**
+	 * 增加http协议头
+	 * @param name http协议头名称
+	 * @param value http协议头值
+	 */
+	public void addHeader(String name, String value) {
+		this.httpGet.addHeader(name, value);
+		this.httpPost.addHeader(name, value);
+	}
 	
-	private void addHeaders(HttpUriRequest request, HashMap<String, String> headers) {
+	/**
+	 * 增加http协议头
+	 * @param header http协议头
+	 */
+	public void addHeader(Header header) {
+		this.httpGet.addHeader(header);
+		this.httpPost.addHeader(header);
+	}
+	
+	/**
+	 * 增加一组http协议头
+	 * @param headers 一组http协议头
+	 */
+	public void addHeaders(Header[] headers) {
 		if (headers != null) {
-			for (String key : headers.keySet()) {
-				request.addHeader(key, headers.get(key));
+			for (Header header : headers) {
+				this.httpGet.addHeader(header);
+				this.httpPost.addHeader(header);
 			}
 		}
 	}
 	
-	private void saveFile(HttpResponse response, String fileName) throws Exception {
+	/**
+	 * 增加一组http协议头
+	 * @param headers 一组http协议头
+	 */
+	public void addHeaders(HashMap<String, String> headers) {
+		if (headers != null) {
+			for (String key : headers.keySet()) {
+				this.httpGet.addHeader(key, headers.get(key));
+				this.httpPost.addHeader(key, headers.get(key));
+			}
+		}
+	}
+	
+	/**
+	 * 移除http协议头
+	 * @param name http协议头名称
+	 */
+	public void removeHeader(String name) {
+		this.httpGet.removeHeaders(name);
+		this.httpPost.removeHeaders(name);
+	}
+	
+	/**
+	 * 移除http协议头
+	 * @param header http协议头
+	 */
+	public void removeHeader(Header header) {
+		this.httpGet.removeHeader(header);
+		this.httpPost.removeHeader(header);
+	}
+	
+	/**
+	 * 移除http协议头
+	 * @param name http协议头名称
+	 * @param value http协议头值
+	 */
+	public void removeHeader(String name, String value) {
+		Header[] headers = this.getAllHeaders();
+		for (Header header : headers) {
+			if(header.getName().equals(name) && header.getValue().equals(value))
+				this.removeHeader(header);
+		}
+	}
+	
+	/**
+	 * 清除所有http协议头
+	 */
+	public void clearAllHeaders() {
+		Header[] headers = this.getAllHeaders();
+		for (Header header : headers) {
+			this.removeHeader(header);
+		}
+	}
+	
+	/**
+	 * 获取所有http协议头
+	 * @return Header[] http协议数组
+	 */
+	public Header[] getAllHeaders() {
+		return this.httpPost.getAllHeaders();
+	}
+	
+	/**
+	 * 将响应内容保存为文件
+	 * @param response 请求的响应
+	 * @param fileName 保存的文件名
+	 * @return String 保存的文件名
+	 * @throws Exception
+	 */
+	private String saveFile(HttpResponse response, String fileName) throws Exception {
 		InputStream inStream = response.getEntity().getContent();
 		File file = new File(fileName);
 		FileOutputStream outputStream = new FileOutputStream(file);
@@ -208,6 +395,8 @@ public class HttpHelper {
 				outputStream.close();
 			}
 		}
+		
+		return fileName;
 	}
 	
 	/**
